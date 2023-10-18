3522963ec589fe6e28818ba743c9a6cee4a8ce37
Lucas M. Lara <LucasML@Live.com>
2023-10-18 11:33:03 -0300

    generalFormInputComponent

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+import { Input, Typography } from "antd";
+import styles from "../app/styles.module.css";
+
+const { Paragraph } = Typography;
+
+interface FeiraInputProps {
+  name: string;
+  label: string;
+  placeholder: string;
+  type: string;
+  antdComponent?: React.ComponentType<any>;
+  required?: boolean;
+}
+
+const FormInput: React.FC<FeiraInputProps> = ({
+  name,
+  label,
+  placeholder,
+  type,
+  antdComponent,
+  required,
+}) => {
+  return (
+    <div>
+      <label className={styles.label} htmlFor={name}>
+        {`${label}:`}
+        {required && (
+          <Paragraph style={{ display: "inline" }} type="danger">
+            {" "}
+            *
+          </Paragraph>
+        )}
+      </label>
+      <Field
+        id={name}
+        as={antdComponent || Input}
+        size="large"
+        name={name}
+        placeholder={placeholder}
+        type={type}
+      />
+      <ErrorMessage name={name}>
+        {(errMsg) => <Paragraph type="danger">{errMsg}</Paragraph>}
+      </ErrorMessage>
+    </div>
+  );
+};
+
+export default FormInput;
