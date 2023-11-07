fb72d9474388fe35502d066efa5ada1b6f309cc4
Lucas M. Lara <LucasML@Live.com>
2023-11-07 14:21:15 -0300

    limpar erros de tipagem para test de build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import "dayjs/locale/zh-cn";
 import FormFeira from "@/components/Form";
-import CryptoExample from "@/utils/crypto";
 
 const WebForm = () => <FormFeira />;
-// const WebForm = () => <CryptoExample />;
 
 export default WebForm;
